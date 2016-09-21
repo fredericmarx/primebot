8579ba04b46ada8432c5cc5b5c7e396295c0c790
Frederic Marx <marx.frederic@gmail.com>
2016-09-21 14:51:06 +0200

    Detect prime numbers

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const getPrimeFactors = require('get-prime-factors')
 const app = express()
 
 app.get('/:id', (req, res, next) => {
   const input = req.originalUrl.substring(1)
 
   if (parseInt(input, 10)) {
-    res.send(`${input} is a number`)
+    if (getPrimeFactors(input).length > 1) {
+      const primeFactors = [...new Set(getPrimeFactors(input))] // Dedupe prime factors
+      res.send(`${input} is divisable by
+        ${primeFactors.length > 1 ? `${primeFactors.slice(0, -1).join(', ')}, and` : ''}
+        ${primeFactors[primeFactors.length - 1]}
+      `)
+    } else {
+      res.send(`${input} is a prime number`)
+    }
   } else {
     res.status(400).send(`${input} is not a number`)
   }
